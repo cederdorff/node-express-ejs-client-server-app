974302f5a25316e48f8bde74e0d88f42118ba7a4
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-08-25 12:50:49 +0200

    feat: enhance form handling by adding age input and error messages, and update rendering logic for improved user feedback

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const app = express(); // Create an instance of express
 app.set("view engine", "ejs"); // Set EJS as the templating engine
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
+const names = [];
+
 // GET - render the index page
 app.get("/", (req, res) => {
-  res.render("index", { name: "" });
+  res.render("index", { name: "", age: "", error: "", names });
 });
 
 // POST - handle form submission
 app.post("/submit", (req, res) => {
-  res.render("index", { name: req.body.name });
+  const name = req.body.name;
+  const age = req.body.age;
+  let error = "";
+  if (!name || name.trim() === "") {
+    error = "Du skal indtaste et navn!";
+  } else if(!age || Number.isNaN(Number(age))) {
+    error = "Du skal indtaste en alder!";
+  }else {
+    names.push(name);
+  }
+  res.render("index", { name, age, error, names });
 });
 
 // Listen on port 3000
